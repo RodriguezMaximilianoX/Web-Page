ca85c56332c41c978588220af5e7a514944acfbd
rodriguezmaximilianoxavier <rodriguezmaximilianoxavier@gmail.com>
2023-09-18 09:49:44 -0300

    finalizando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,16 @@ function Footer() {
   return (
     <footer className='bg-blue-950'>
       <div className='flex justify-around'>
-        <img className='w-12' src= { GH } alt='github'></img>
-        <img className='w-12' src= { LinkedIn } alt='linkedin'></img>
-        <img className='w-12' src= { Instagram } alt='instagram'></img>
-      </div>  
+        <a href='https://github.com/RodriguezMaximilianoX' target='_blank' rel="noreferrer">
+          <img className='w-12' src={GH} alt='GitHub'></img>
+        </a>
+        <a href='https://www.linkedin.com/in/rodriguez-maximiliano-xavier/https://www.linkedin.com/in/rodriguez-maximiliano-xavier/' target='_blank' rel='noreferrer'>
+          <img className='w-12' src={LinkedIn} alt='linkedin'></img>
+        </a>
+        <a href='https://www.instagram.com/rmx_dev/' target='_blank' rel='noreferrer'>
+          <img className='w-12' src={Instagram} alt='instagram'></img>
+        </a>
+      </div>
     </footer>
   );
 }
