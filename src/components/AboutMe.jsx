26b1217d8c90b3bc3d0325ea7ed0c0dff2d41e55
rodriguezmaximilianoxavier <rodriguezmaximilianoxavier@gmail.com>
2023-09-18 10:22:27 -0300

    agregando icono web

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ import Svg from '../assets/icons/programming.svg';
 
 export default function AboutMe() {
   return (
-    <section className='flex items-center justify-center'>
+    <section className='flex items-center justify-center mt-10'>
       <img src={Svg} alt='banner' className='w-96 my-4 mr-52' />
       <div className='flex flex-col'>
         <h2 className='text-5xl font-bold mb-14'>¡Hola!</h2>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import '../App.css';
 
 function Navbar() {
   return (
-    <nav className='flex flex-row justify-between items-center bg-blue-950 py-5'>
+    <nav className='flex flex-row justify-between items-center bg-blue-950 py-5 fixed top-0 left-0 right-0'>
       <h1 className='text-3xl font-bold ml-48 text-white'>RMX Desarrollador FullStack</h1>
       <ul className='flex flex-row mr-10'>
         <li className='px-4 text-lg text-white'><a href="#about">Sobre Mí</a></li>
